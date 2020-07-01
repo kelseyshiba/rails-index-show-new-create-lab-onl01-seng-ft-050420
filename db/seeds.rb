89840e5580c8898b798415661142346a9ad0db34
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-01 00:25:52 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+25.times do 
+    Coupon.create(coupon_code: Faker::Code.sin, store: Faker::Company.name)
+end
+
+p 'Coupons created!'
